fd1eabc83800d712c9640b7ac9e1bdf9b0adcbd0
thicchead <metehan.karakoruk@gmail.com>
2022-06-24 00:21:55 +0200

    First commit

---
+++ misc/one.py
@@ -0,0 +1,21 @@
+# print("Welkom bij de herexamens")
+# print('Welkom bij de herexamens')
+
+# print("Mijn", "naam", "is", "Metehan")
+
+# print("dia's")
+# print('dia\'s')
+
+# print("Metehan zegt: \"Hallo!\"")
+
+# print("\\\"Hallo\"")
+
+# print(3.14159265)
+# print(3,14159265)
+
+# 5 // 3 = 1 --> hoeveel keer gaat het erin
+# 5 % 3 = 2  --> hoeveel is de rest
+# float in de deling ergens --> uitkomst is ook altijd een float
+# bv 6.0 / 2 = 3.2
+
+
