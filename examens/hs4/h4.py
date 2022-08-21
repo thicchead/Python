905148faccc7a569912f9842162bb17f32eb0d80
thicchead <metehan.karakoruk@gmail.com>
2022-08-21 20:26:45 +0200

    h4 klaar

---
+++ examens/hs4/h4.py
@@ -0,0 +1,37 @@
+# Eerste vb oefening
+"""totaal = 0
+
+for i in range(28):
+    leeftijd = int(input("Leeftijd student " + str(i + 1) + ": "))
+    totaal += leeftijd
+
+print(totaal / 28.0)"""
+
+# Andere oef
+"""for i in range(1, 6, 2):
+    print(i)
+    print("De waarde van het kwadraat van " + str(i) + " is " + str(i ** 2))
+print()
+print("De waarde van i na afloop van de for lus is" + str(i))"""
+
+# While oefening
+"""getal = int(input("Getal = "))
+
+while getal != 0:
+    print(getal ** 2)
+    getal = int(input("Getal = "))"""
+"""getal = int(input("Getal = "))
+
+while getal > 10 or getal < 0:
+    print("Fout")
+    getal = int(input("Getal = "))"""
+
+# Nested loop
+"""for i in range(1, 11):
+    for j in range(1, 11):
+        print(i * j, end= " ")
+    print()"""
+"""for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("a", end=" ")
+    print()"""
---
+++ examens/hs4/h4_oef.py
@@ -0,0 +1,25 @@
+"""getal = int(input("Getal = "))
+
+for i in range(1, 21):
+    print(str(i) + " x " + str(getal) + " = " + str(i * getal))"""
+"""hoogste_temperatuur = -100
+totaal = 0
+for i in range(10):
+    temperatuur = float(input("Gemeten temperatuur op dag " + str(i + 1) + " = "))
+    totaal += temperatuur
+
+    if temperatuur > hoogste_temperatuur:
+        hoogste_temperatuur = temperatuur
+
+print("Gemiddelde temperatuur = " + str(totaal / 10))
+print("Hoogste temperatuur = " + str(hoogste_temperatuur))"""
+"""for i in range(2, 6):
+    for j in range(1, 11):
+        print(str(i) + " x " + str(j) + " = " + str(i * j), end=" | ")
+    print()"""
+"""grootte = int(input("Grootte driehoek = "))
+
+for i in range(grootte, 0, -1):
+    for j in range(i, 0, -1):
+        print("@", end=" ")
+    print()"""
